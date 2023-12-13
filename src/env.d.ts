87fa8d89e797d2b4220b2fa772f1549469c0f4a7
siat.testing <siat.testing@gmail.com>
2023-12-13 22:25:11 +0000

    initial

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="astro/client" />
+declare namespace App {
+  interface Locals {
+    auth: import('lucia').AuthRequest;
+  }
+}
+
+/// <reference types="lucia" />
+declare namespace Lucia {
+  type Auth = import('./lib/lucia').Auth;
+  type DatabaseUserAttributes = {
+    username: string;
+    email: string;
+    email_verified: boolean;
+  };
+  type DatabaseSessionAttributes = {};
+}
