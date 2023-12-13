099d18458d5df8ed69b6b1bca90ca6eac52ef4f2
siat.testing <siat.testing@gmail.com>
2023-12-13 22:36:03 +0000

    initial

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,12 +6,12 @@ declare namespace App {
 }
 
 /// <reference types="lucia" />
-declare namespace Lucia {
-  type Auth = import('./lib/lucia').Auth;
-  type DatabaseUserAttributes = {
-    username: string;
-    email: string;
-    email_verified: boolean;
-  };
-  type DatabaseSessionAttributes = {};
-}
+// declare namespace Lucia {
+//   type Auth = import('./lib/lucia').Auth;
+//   type DatabaseUserAttributes = {
+//     username: string;
+//     email: string;
+//     email_verified: boolean;
+//   };
+//   type DatabaseSessionAttributes = {};
+// }
