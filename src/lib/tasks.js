0b8693469716c04fbb8bff0358d9001c1691703b
siat.testing <siat.testing@gmail.com>
2023-12-28 21:46:07 +0000

    task crud

--- src/lib/tasks.js
+++ src/lib/tasks.js
@@ -1,24 +1,46 @@
 import { db, replicatedDB } from './db';
 
 export const mytasks = [
-  { id: 1, title: 'created basic skeleton' },
-  { id: 2, title: 'added lucia with pnpm add lucia' },
-  { id: 3, title: 'added via pnpm add drizzle-orm postgres' },
+  { id: 10011, title: 'created basic skeleton', completed: false },
+  { id: 10012, title: 'added lucia with pnpm add lucia', completed: false },
   {
-    id: 4,
+    id: 10013,
+    title: 'added via pnpm add drizzle-orm postgres',
+    completed: false,
+  },
+  {
+    id: 10014,
     title:
       'added Lucia adapter for sqlite to try with Turso: pnpm add @lucia-auth/adapter-sqlite',
+    completed: false,
   },
   {
-    id: 5,
+    id: 10015,
     title:
       'added @linsql/client as per the docs for Turso on drizzle orm website',
+    completed: false,
   },
-  { id: 6, title: 'Configured env.d.ts as per Lucia documentation for Astro' },
-  { id: 7, title: 'Added the middleware file in src/middleware.ts' },
-  { id: 8, title: 'Removed Turso and drizzle and postgres' },
-  { id: 9, title: 'Added prisma via npm' },
-  { id: 10, title: 'Added vercel adapter with npx astro add vercel' },
+  {
+    id: 10016,
+    title: 'Configured env.d.ts as per Lucia documentation for Astro',
+    completed: false,
+  },
+  {
+    id: 10017,
+    title: 'Added the middleware file in src/middleware.ts',
+    completed: false,
+  },
+  {
+    id: 10018,
+    title: 'Removed Turso and drizzle and postgres',
+    completed: false,
+  },
+  { id: 10019, title: 'Added prisma via npm', completed: false },
+  {
+    id: 10020,
+    title: 'Added vercel adapter with npx astro add vercel',
+    completed: false,
+  },
 ];
 
 await replicatedDB.sync();
