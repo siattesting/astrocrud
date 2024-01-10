d37b65ad3326f9defac106f50fd98c8d289f7f62
siat.testing <siat.testing@gmail.com>
2024-01-10 21:22:02 +0000

    task crud

--- src/lib/UserServices.ts
+++ src/lib/UserServices.ts
@@ -15,3 +15,18 @@ export const getUser = async (userId: string) => {
     console.error(error);
   }
 };
+export const getUserByEmail = async (email: string) => {
+  // Retrieve the user's details and points in the database
+  try {
+    const rs = await db.execute({
+      sql: 'SELECT * FROM user_auth WHERE email = ?',
+      args: [email],
+    });
+    // console.log(rs.rows);
+    return {
+      user: rs.rows[0],
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
